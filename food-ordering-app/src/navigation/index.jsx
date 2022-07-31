cb3fa53e17363cee2c845ec7ed63c2886d03cede
ivan hb <>
2022-07-31 09:47:56 +0000

    Initial commit

---
+++ food-ordering-app/src/navigation/index.jsx
@@ -0,0 +1,13 @@
+// This is for routing purposes
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+// the component
+const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  )
+}
+
+export default Navigation;
